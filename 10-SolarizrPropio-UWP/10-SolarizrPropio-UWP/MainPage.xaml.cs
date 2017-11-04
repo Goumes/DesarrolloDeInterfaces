7ccd3744cf691030d83d9e075e081255006e1247
Alejandro Gómez Olivera <alejandro_golivera@hotmail.com>
2017-11-04 20:46:41 +0100

    Nuevo Solarizr
    Faltan algunas cosas pero lo importante funciona perfectamente.
    De momento pasando de terminar esto...

---
+++ 10-SolarizrPropio-UWP/10-SolarizrPropio-UWP/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0xc0a
+
+namespace _10_SolarizrPropio_UWP
+{
+    /// <summary>
+    /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void clickBtn_Navegar(object sender, RoutedEventArgs e)
+        {
+            String boton = ((Button)sender).Name;
+
+            switch (boton)
+            {
+                case "btnCasas":
+                    miFrame.Navigate(typeof(Casas));
+                    break;
+                case "btnUsers":
+                    Frame.Navigate(typeof(Login));
+                    break;
+            }
+        }
+
+        private void toggle_Click(object sender, RoutedEventArgs e)
+        {
+            miSplitView.IsPaneOpen = !miSplitView.IsPaneOpen;
+        }
+
+    }
+}
