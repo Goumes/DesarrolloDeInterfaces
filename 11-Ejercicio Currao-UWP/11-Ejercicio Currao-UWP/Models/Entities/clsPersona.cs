d658c7005e4a883e699846cd87850ffc018de6ed
Alejandro Gómez Olivera <alejandro_golivera@hotmail.com>
2017-11-09 12:57:38 +0100

    Ejercicio Currao
    Falta la ultima parte. Poner el formulario debajo de la lista cuando hagas la pantalla más pequeña

---
+++ 11-Ejercicio Currao-UWP/11-Ejercicio Currao-UWP/Models/Entities/clsPersona.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05_PersonaModificada_ASP.Models.Entities
+{
+    public class clsPersona
+    {
+        public int idPersona { get; set; }
+        public string nombre { get; set; }
+        public string apellido1 { get; set; }
+        public string apellido2 { get; set; }
+        public string direccion { get; set; }
+        public DateTime fechaNac { get; set; }
+        public string telefono { get; set; }
+
+        public clsPersona()
+        {
+            this.idPersona = 0;
+            this.nombre = "Alejandro";
+            this.apellido1 = "Gómez";
+            this.apellido2 = "Olivera";
+            this.fechaNac = new DateTime();
+            this.direccion = "Mi casa";
+            this.telefono = "954224444";
+        }
+
+        public clsPersona(int idPersona, String nombre, String apellido1, String apellido2, DateTime fechaNac, String direccion, String telefono)
+        {
+            this.idPersona = idPersona;
+            this.nombre = nombre;
+            this.apellido1 = apellido1;
+            this.apellido2 = apellido2;
+            this.fechaNac = fechaNac;
+            this.direccion = direccion;
+            this.telefono = telefono;
+        }
+
+    }
+}
+
+  
---
+++ 11-Ejercicio Currao-UWP/11-Ejercicio Currao-UWP/ViewModels/clsMainPageVM.cs
@@ -0,0 +1,72 @@
+﻿using _05_PersonaModificada_ASP.Models.Entities;
+using _11_Ejercicio_Currao_UWP.Models.Lists;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_Ejercicio_Currao_UWP.ViewModels
+{
+    class clsMainPageVM : INotifyPropertyChanged
+    {
+        #region "Atributos"
+        private ObservableCollection<clsPersona> _listadoPersonas;
+        private clsPersona _personaSeleccionada;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+
+        #region "Constructores"
+        public clsMainPageVM ()
+        {
+            clsListadoPersonas personas = new clsListadoPersonas();
+            _listadoPersonas = personas.personas;
+        }
+        #endregion
+
+        #region "Propiedades"
+
+        public ObservableCollection<clsPersona> listado
+        {
+            get
+            {
+                return _listadoPersonas;
+            }
+        }
+
+        public clsPersona personaSeleccionada
+        {
+            get
+            {
+                return _personaSeleccionada;
+            }
+
+            set
+            {
+                _personaSeleccionada = value;
+               NotifyPropertyChanged("personaSeleccionada");
+            }
+        }
+        #endregion
+
+        #region "Metodos"
+
+        // This method is called by the Set accessor of each property.
+        // The CallerMemberName attribute that is applied to the optional propertyName
+        // parameter causes the property name of the caller to be substituted as an argument.
+
+        private void NotifyPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+        #endregion
+    }
+}
