7107795a05b865a370cfc526a30f943ffad54d56
Alejandro Gómez Olivera <alejandro_golivera@hotmail.com>
2017-12-01 14:24:58 +0100

    Estoy ofuscado con visual

---
+++ 19-CRUD-Personas/19-CRUD-Personas-ET/clsListadoPersonas.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19_CRUD_Personas_ET
+{
+    public class clsListadoPersonas
+    {
+        public ObservableCollection<clsPersona> personas { get; set; }
+
+        public clsListadoPersonas()
+        {
+            this.personas = new ObservableCollection<clsPersona>();
+            this.cargar();
+        }
+
+        public clsListadoPersonas(ObservableCollection<clsPersona> personas)
+        {
+            this.personas = personas;
+        }
+
+        public void cargar()
+        {
+            this.personas.Add(new clsPersona(1, "pepejava1", "apellido1_1", "apellido2_1", new DateTime(2001, 1, 1), "direccion1", "111111111"));
+            this.personas.Add(new clsPersona(2, "pepejava2", "apellido1_2", "apellido2_2", new DateTime(2002, 2, 2), "direccion2", "222222222"));
+            this.personas.Add(new clsPersona(3, "pepejava3", "apellido1_3", "apellido2_3", new DateTime(2003, 3, 3), "direccion3", "333333333"));
+            this.personas.Add(new clsPersona(4, "pepejava4", "apellido1_4", "apellido2_4", new DateTime(2004, 4, 4), "direccion4", "444444444"));
+            this.personas.Add(new clsPersona(5, "pepejava5", "apellido1_5", "apellido2_5", new DateTime(2005, 5, 5), "direccion5", "555555555"));
+            this.personas.Add(new clsPersona(6, "pepejava6", "apellido1_6", "apellido2_6", new DateTime(2006, 6, 6), "direccion6", "666666666"));
+            this.personas.Add(new clsPersona(7, "pepejava7", "apellido1_7", "apellido2_7", new DateTime(2007, 7, 7), "direccion7", "777777777"));
+            this.personas.Add(new clsPersona(8, "pepejava8", "apellido1_8", "apellido2_8", new DateTime(2008, 8, 8), "direccion8", "888888888"));
+            this.personas.Add(new clsPersona(9, "pepejava9", "apellido1_9", "apellido2_9", new DateTime(2009, 9, 9), "direccion9", "999999999"));
+            this.personas.Add(new clsPersona(10, "pepejava10", "apellido1_10", "apellido2_10", new DateTime(2010, 10, 10), "direccion10", "101010101"));
+            this.personas.Add(new clsPersona(11, "pepejava11", "apellido1_11", "apellido2_11", new DateTime(2011, 11, 11), "direccion11", "110110110"));
+            this.personas.Add(new clsPersona(12, "pepejava12", "apellido1_12", "apellido2_12", new DateTime(2012, 12, 12), "direccion12", "121212121"));
+        }
+    }
+}
---
+++ 19-CRUD-Personas/19-CRUD-Personas_UI/clsMainPageVM.cs
@@ -0,0 +1,243 @@
+﻿using _19_CRUD_Personas_ET;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19_CRUD_Personas_UI
+{
+    class clsMainPageVM : clsVMBase
+    {
+        #region "Atributos"
+        private ObservableCollection<clsPersona> _listadoPersonas;
+        private ObservableCollection<clsPersona> _listadoAuxiliar;
+        private clsPersona _personaSeleccionada;
+        private String _textoBusqueda;
+        private DelegateCommand _borrarCommand;
+        private DelegateCommand _saveCommand;
+        private DelegateCommand _addCommand;
+        private DelegateCommand _buscarCommand;
+
+        #endregion
+
+        #region "Constructores"
+        public clsMainPageVM()
+        {
+            clsListadoPersonas personas = new clsListadoPersonas();
+            _listadoPersonas = personas.personas;
+            _listadoAuxiliar = personas.personas;
+            _textoBusqueda = "";
+        }
+        #endregion
+
+        #region "Propiedades"
+
+        public ObservableCollection<clsPersona> listado
+        {
+            get
+            {
+                return _listadoPersonas;
+            }
+        }
+
+        public ObservableCollection<clsPersona> listadoAuxiliar
+        {
+            get
+            {
+                return _listadoAuxiliar;
+            }
+
+            set
+            {
+                _listadoAuxiliar = value;
+            }
+        }
+
+        public clsPersona personaSeleccionada
+        {
+            get
+            {
+                return _personaSeleccionada;
+            }
+
+            set
+            {
+                _personaSeleccionada = value;
+                _borrarCommand.RaiseCanExecuteChanged();
+                _saveCommand.RaiseCanExecuteChanged();
+                NotifyPropertyChanged("personaSeleccionada");
+            }
+        }
+
+        public String textoBusqueda
+        {
+            get
+            {
+                return _textoBusqueda;
+            }
+
+            set
+            {
+                _textoBusqueda = value;
+                _buscarCommand.RaiseCanExecuteChanged();
+                //NotifyPropertyChanged("textoBusqueda");
+            }
+        }
+
+        public DelegateCommand borrarCommand
+        {
+            get
+            {
+                _borrarCommand = new DelegateCommand(borrar, puedeBorrar); // (execute (), canExecute ())
+                return _borrarCommand;
+            }
+
+            set
+            {
+                _borrarCommand = value;
+            }
+        }
+
+        public DelegateCommand saveCommand
+        {
+            get
+            {
+                _saveCommand = new DelegateCommand(save, canSave);
+                return _saveCommand;
+            }
+
+            set
+            {
+                _saveCommand = value;
+            }
+        }
+
+        public DelegateCommand addCommand
+        {
+            get
+            {
+                _addCommand = new DelegateCommand(add);
+                return _addCommand;
+            }
+
+            set
+            {
+                _addCommand = value;
+            }
+        }
+
+        public DelegateCommand buscarCommand
+        {
+            get
+            {
+                _buscarCommand = new DelegateCommand(buscar, puedeBuscar);
+                return _buscarCommand;
+            }
+
+            set
+            {
+                _buscarCommand = value;
+            }
+        }
+        #endregion
+
+        #region Metodos
+        /// <summary>
+        /// Metodo dedicado a comprobar si se puede borrar un item
+        /// </summary>
+        /// <returns>boolean</returns>
+        public bool puedeBorrar()
+        {
+            bool puede = false;
+
+            if (_personaSeleccionada != null)
+            {
+                puede = true;
+            }
+
+            return puede;
+        }
+
+        /// <summary>
+        /// Metodo dedicado a borrar un item de la lista
+        /// </summary>
+        public void borrar()
+        {
+            listado.Remove(_personaSeleccionada);
+        }
+
+        /// <summary>
+        /// Metodo que comprueba si se puede guardar
+        /// </summary>
+        /// <returns>boolean</returns>
+        public bool canSave()
+        {
+            bool puede = false;
+
+            if (_personaSeleccionada != null)
+            {
+                puede = true;
+            }
+
+            return puede;
+        }
+
+        /// <summary>
+        /// Metodo dedicado a guardar una nueva persona creada
+        /// </summary>
+        public void save()
+        {
+            if (_personaSeleccionada.idPersona == 0)
+            {
+                _personaSeleccionada.idPersona = listado.ElementAt(listado.Count - 1).idPersona + 1;
+                NotifyPropertyChanged("personaSeleccionada");
+                listado.Add(_personaSeleccionada);
+                NotifyPropertyChanged("listado");
+            }
+        }
+
+        /// <summary>
+        /// Metodo que crea una persona nueva
+        /// </summary>
+        public void add()
+        {
+            _personaSeleccionada = new clsPersona();
+            NotifyPropertyChanged("personaSeleccionada");
+        }
+
+        public void buscar()
+        {
+            listadoAuxiliar = new ObservableCollection<clsPersona>();
+
+            for (int i = 0; i < listado.Count; i++)
+            {
+                if (listado[i].nombre.ToLower().Contains(textoBusqueda.ToLower()))
+                {
+                    listadoAuxiliar.Add(listado[i]);
+                    NotifyPropertyChanged("listadoAuxiliar");
+                }
+            }
+        }
+
+        public bool puedeBuscar()
+        {
+            bool puede = false;
+
+            if (textoBusqueda.Length > 0)
+            {
+                puede = true;
+            }
+
+            else
+            {
+                _listadoAuxiliar = listado;
+                NotifyPropertyChanged("listadoAuxiliar");
+            }
+
+            return puede;
+        }
+        #endregion
+    }
+}
